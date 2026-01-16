914b732e57199daab609aadd8ec6643a75e3c4c0
sachin11p12 <sachin11p12@gmail.com>
2026-01-16 17:11:54 +0530

    Add frontend

---
+++ src/components/students/CalltoAction.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CalltoAction = () => {
+  return (
+    <div>
+      <h1>Call to Action</h1>
+    </div>
+  );
+};
+
+export default CalltoAction;
---
+++ src/components/students/companies.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const companies = () => {
+  return (
+    <div>
+      <h1>Companies</h1>
+    </div>
+  );
+};
+
+export default companies;
