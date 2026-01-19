f226fc05afdaf06a088b9a58f5760e4e30636b62
sachin11p12 <sachin11p12@gmail.com>
2026-01-19 18:56:47 +0530

    Footer completed , hero Page completed

--- src/components/students/Footer.jsx
+++ src/components/students/Footer.jsx
@@ -17,13 +17,36 @@ const Footer = () => {
           <ul className="flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2">
             <li>
               <a href="#">Home</a>
+            </li>
+            <li>
               <a href="#">About us</a>
+            </li>
+            <li>
               <a href="#">Contact us</a>
+            </li>
+            <li>
               <a href="#">Privacy Policy</a>
             </li>
           </ul>
         </div>
-        <div></div>
+        <div className="hidden md:flex flex-col items-start w-full">
+          <h2 className="fonr-semibold text-white mb-5">
+            Subscribe to our newsletter
+          </h2>
+          <p className="text-sm text-white/80">
+            The latest news, articles, and resources, sent to your inbox weekly.
+          </p>
+          <div className="flex items-center gap-2 pt-4">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="border border-gray-500/30 bg-gray-800 text-gray-500 placeholder:text-gray-500 outline-none w-64 h-9 rounded px-2 text-sm"
+            />
+            <button className="text-white bg-blue-600 w-24 h-9 rounded">
+              Subscribe
+            </button>
+          </div>
+        </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm text-white/60">
         Copyright © 2026 Sachin Tiwari. All Rights Reserved.
