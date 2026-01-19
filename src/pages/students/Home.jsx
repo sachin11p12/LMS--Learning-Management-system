c3a57fbc429b801cbc919d326f7ac197b6de7667
sachin11p12 <sachin11p12@gmail.com>
2026-01-19 17:17:04 +0530

    Testimonial section add

--- src/pages/students/Home.jsx
+++ src/pages/students/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../../components/students/Hero";
 import Companies from "../../components/students/companies";
 import CourseSection from "../../components/students/CourseSection";
+import TestimonialSection from "../../components/students/TestimonialSection";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <Companies />
       <CourseSection />
+      <TestimonialSection /> 
     </div>
   );
 };
