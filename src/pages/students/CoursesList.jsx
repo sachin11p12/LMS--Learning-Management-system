fe02b5f420ad62829dc348a8afef29ecb2edf6f3
sachin11p12 <sachin11p12@gmail.com>
2026-01-20 10:50:43 +0530

    Initiated course-List page , add searchBar and head

--- src/pages/students/CoursesList.jsx
+++ src/pages/students/CoursesList.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import SearchBar from "../../components/students/SearchBar";
+import { useParams } from "react-router-dom";
 const CoursesList = () => {
+  const { navigate } = useContext(AppContext);
+  const {input} = useParams()
   return (
-    <div>
-      <h1>Courses List Page</h1>
-    </div>
+    <>
+      <div className="relative md:px-36 px-8 pt-20 text-left">
+        <div className="flex md:flex-row flex-col gap-6 items-start justify-between w-full">
+          <div>
+            <h1 className="text-4xl font-semibold text-gray-800">Course List</h1>
+            <p className="text-gray-500">
+              <span
+                className="text-blue-600 cursor-pointer"
+                onClick={() => navigate("/")}
+              >
+                Home
+              </span>{" "}
+              / <span>Course List</span>
+            </p>
+          </div>
+          <SearchBar data={input} />
+        </div>
+      </div>
+    </>
   );
 };
 
