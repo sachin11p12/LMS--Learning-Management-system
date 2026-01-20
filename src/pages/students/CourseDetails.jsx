36ec99751f683d071f650efd763d9564a6ec9e91
sachin11p12 <sachin11p12@gmail.com>
2026-01-20 12:00:06 +0530

    add custom tailwind utility class and initaiated coursedetails page

--- src/pages/students/CourseDetails.jsx
+++ src/pages/students/CourseDetails.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
 
 const CourseDetails = () => {
+  const { id } = useParams();
+  const [courseData, setCourseData] = useState(null);
+  const { allCourse } = useContext(AppContext);
+  const fetchCourseData = async () => {
+    const findCourse = allCourse.find((course) => course._id === id);
+    setCourseData(findCourse);
+  };
+  useEffect(() => {
+    fetchCourseData();
+  }, []);
   return (
-    <div>
-      <h1>Course Details Page</h1>
+    <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left ">
+      <div className="absolute top-0 left-0 w-full h-section-height bg-linear-to-b from-cyan-100/70"></div>
+      {/* left column */}
+      <div>
+        <h1>{courseData.courseTitle}</h1>
+      </div>
+      {/* right column */}
+      <div></div>
     </div>
   );
 };
