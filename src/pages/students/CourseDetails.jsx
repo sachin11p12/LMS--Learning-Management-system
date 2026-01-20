1636666099dfb4d77b1d17a15eabc683d6e3e9f0
sachin11p12 <sachin11p12@gmail.com>
2026-01-20 12:59:53 +0530

    Title and description added successfuly

--- src/pages/students/CourseDetails.jsx
+++ src/pages/students/CourseDetails.jsx
@@ -1,28 +1,69 @@
 import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../../context/AppContext";
+import Loading from "../../components/students/Loading";
+import { assets } from "../../assets/assets";
 
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
-  const { allCourse } = useContext(AppContext);
+  const { allCourses, calculaterating } = useContext(AppContext);
   const fetchCourseData = async () => {
-    const findCourse = allCourse.find((course) => course._id === id);
+    const findCourse = allCourses.find((course) => course._id === id);
     setCourseData(findCourse);
   };
   useEffect(() => {
     fetchCourseData();
-  }, []);
-  return (
-    <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left ">
-      <div className="absolute top-0 left-0 w-full h-section-height bg-linear-to-b from-cyan-100/70"></div>
-      {/* left column */}
-      <div>
-        <h1>{courseData.courseTitle}</h1>
+  }, [allCourses, id]);
+  return courseData ? (
+    <>
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left ">
+        <div className="absolute top-0 left-0 w-full h-section-height bg-linear-to-b from-cyan-100/70"></div>
+        {/* left column */}
+        <div className="max-w-xl z-10 text-gray-500">
+          <h1 className="md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800">
+            {courseData.courseTitle}
+          </h1>
+          <p
+            className="pt-4 md:text-base text-sm"
+            dangerouslySetInnerHTML={{
+              __html: courseData.courseDescription.slice(0, 200),
+            }}
+          ></p>
+
+          <div className="flex items-center space-x-2 pt-3 pb-1 text-sm">
+            <p>{calculaterating(courseData)}</p>
+            <div className="flex">
+              {[...Array(5)].map((_, i) => (
+                <img
+                  key={i}
+                  src={
+                    i < Math.floor(calculaterating(courseData))
+                      ? assets.star
+                      : assets.star_blank
+                  }
+                  alt=""
+                  className="w-3.5 h-3.5"
+                />
+              ))}
+            </div>
+            <p className="text-gray-500">
+              ({courseData.courseRatings.length}{" "}
+              {courseData.courseRatings.length > 1 ? "ratings" : "rating"})
+            </p>
+            <p>
+              {courseData.enrolledStudents.length}{" "}
+              {courseData.enrolledStudents.length > 1 ? "students" : "student"}
+            </p>
+          </div>
+          <p className="text-sm">Course by <span className="text-blue-600 underline cursor-pointer">Sachin Tiwari </span></p>
+        </div>
+        {/* right column */}
+        <div></div>
       </div>
-      {/* right column */}
-      <div></div>
-    </div>
+    </>
+  ) : (
+    <Loading />
   );
 };
 
