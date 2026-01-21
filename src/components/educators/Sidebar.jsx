2cf16e5b93b884f4c9469d625ec3b2ac54892d40
sachin11p12 <sachin11p12@gmail.com>
2026-01-21 17:42:09 +0530

    sidebar for educator

--- src/components/educators/Sidebar.jsx
+++ src/components/educators/Sidebar.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import { assets } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-  return (
-    <div>
-      <h1>Edu - sidebar</h1>
+
+  const {isEducator} = useContext(AppContext);
+
+  const menuItems = [
+    {name:'Dashboard', path: '/educator', icon: assets.home_icon},
+    {name:'Add Course', path: '/educator/add-course', icon: assets.add_icon},
+    {name:'My Courses', path: '/educator/my-courses', icon: assets.my_course_icon},
+    {name:'Student Enrolled', path: '/educator/student-enrolled', icon: assets.person_tick_icon},
+  ];
+  return isEducator&& (
+    <div className="md:w-64 w-16 border-r min-h-screen text-base border-gray-500 py-2 flex flex-col">
+      {menuItems.map((item)=>{
+        <NavLink
+        to={item.path}
+        key={item.name}
+        end={item.path ==="/Educato"}
+        >
+          <img src={item.icon} alt=""className="w-6 h-6" />
+          <p className="md:block hidden text-cen'">{item.name}</p> 
+        </NavLink>
+      })}
     </div>
   );
 };
--- src/pages/educators/Educators.jsx
+++ src/pages/educators/Educators.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/educators/Navbar";
+import Sidebar from "../../components/educators/Sidebar";
 const Educator = () => {
   return (
     <div className="text-default min-h-screen bg-white">
       <Navbar />
-      <div>{<Outlet />}</div>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1">
+          {<Outlet />}
+        </div>
+      </div>
     </div>
   );
 };
