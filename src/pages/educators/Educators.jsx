5551a26d887d143af5d957f030fb63177cbd684f
sachin11p12 <sachin11p12@gmail.com>
2026-01-21 19:08:50 +0530

    add footer in educator

--- src/pages/educators/Educators.jsx
+++ src/pages/educators/Educators.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/educators/Navbar";
 import Sidebar from "../../components/educators/Sidebar";
+import Footer from "../../components/educators/Footer";
 const Educator = () => {
   return (
     <div className="text-default min-h-screen bg-white">
@@ -12,6 +13,7 @@ const Educator = () => {
           {<Outlet />}
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
