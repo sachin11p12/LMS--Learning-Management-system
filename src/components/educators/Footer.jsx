5bc7575188ad1f18c5632139b669356dc1d08519
sachin11p12 <sachin11p12@gmail.com>
2026-01-22 20:11:59 +0530

    sComment

--- src/components/educators/Footer.jsx
+++ src/components/educators/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
         <img className="hidden md:block w-20" src={assets.logo} alt="logo" />
         <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
         <p className="py-4 text-center text-xs md:text-sm text-gray-500">
-          Copyright © 2026 Sachin Tiwari. All rights reserved.
+          Copyright © 2026 Sachin Tiwari. 
         </p>
       </div>
-      {/* <div className="flex items-center gap-3 max-md:mt-4">
+      <div className="flex items-center gap-3 max-md:mt-4">
         <a href="">
           <img src={assets.facebook_icon} alt="facebook_icon" />
         </a>
@@ -21,7 +21,7 @@ const Footer = () => {
         <a href="">
           <img src={assets.instagram_icon} alt="instagram_icon" />
         </a>
-      </div> */}
+      </div>
     </footer>
   );
 };
