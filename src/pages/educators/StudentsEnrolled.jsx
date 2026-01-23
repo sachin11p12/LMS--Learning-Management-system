488cab89b773c27aaa756c133fef16974c827c67
sachin11p12 <sachin11p12@gmail.com>
2026-01-23 13:36:19 +0530

    Student enrolled page initiated

--- src/pages/educators/StudentsEnrolled.jsx
+++ src/pages/educators/StudentsEnrolled.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dummyStudentEnrolled } from "../../assets/assets";
+import Loading from "../../components/students/Loading";
 
 const StudentsEnrolled = () => {
-  return (
-    <div>
-      <h1>StudentsEnrolled</h1>
+  const [enrolledStudents, setEnrolledStudents] = useState(null)
+
+  const fetchEnrolledStudents = async () => {
+    setEnrolledStudents(dummyStudentEnrolled)
+  }
+  useEffect(()=> {
+    fetchEnrolledStudents()
+  },[])
+  return enrolledStudents ? (
+    <div >
+      <div>
+        <table>
+          <thead className="text-gray-900 border-b border-gray-500/20 text-sm text-left">
+          <tr>
+            <th className="px-4 py-3 font-semibold text-center hidden sm:table-cell">#</th>
+            <th className="px-4 py-3 font-semibold">Student Name</th>
+            <th className="px-4 py-3 font-semibold">Course Title</th>
+            <th className="px-4 py-3 font-semibold hidden sm:table-cell">Date</th>
+          </tr>
+
+          </thead>
+        </table>
+      </div>
     </div>
-  );
+  ) : <Loading />
 };
 
 export default StudentsEnrolled;
